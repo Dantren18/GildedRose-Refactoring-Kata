75cd767414dff49da3115bc7ef414750d8471f1f
David Renwick <david.renwick@bjss.com>
2023-07-13 10:42:02 +0100

    Added tests to tests for quality decreasing correctly

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,15 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
+  it('should decrease quality and sellIn for normal item', () => {
+    const gildedRose = new GildedRose([new Item('foo', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(19);
+    expect(items[0].sellIn).toBe(9);
+  });
+  it('should degrade quality twice as fast once sell by date has passed', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(18);
+  });
 });
